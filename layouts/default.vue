41bc7cbcd724715afb9368a15ee1d5e5eb5a81db
ellin.hand <ellin.hand@kakaocorp.com>
2024-05-05 00:16:43 +0900

    #update: Nuxt 컴포넌트 계층

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-layout view="hHh lpR fFf" class="bg-grey-2">
+    <q-header elevated class="bg-dark text-white">
+      <q-toolbar>
+        <q-toolbar-title> Vue & Nuxt Mastery Class </q-toolbar-title>
+        <q-btn stretch flat label="Home" to="/" />
+        <q-separator dark vertical />
+        <q-btn stretch flat label="About" to="/about" />
+      </q-toolbar>
+    </q-header>
+    <q-page-container :style="pageContainerStyle">
+      <slot></slot>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+const pageContainerStyle = computed(() => ({
+  maxWidth: '1080px',
+  margin: '0 auto',
+}));
+</script>
