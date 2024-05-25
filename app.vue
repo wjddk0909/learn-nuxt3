aec83fcfed1a3a9d0053b6c11e72fdfc1f5441e2
wjddk0909 <fjddkcb@naver.com>
2024-05-25 11:01:17 +0900

    update: NuxtApp provide

--- app.vue
+++ app.vue
@@ -5,4 +5,15 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const nuxtApp = useNuxtApp();
+console.log('nuxtApp', nuxtApp);
+
+// vue application instance
+// nuxtApp.vueApp.component();
+// nuxtApp.vueApp.directive();
+// nuxtApp.vueApp.use();
+
+// provide 활용
+// nuxtApp.provide('hello', (msg: string) => `Hello ${msg}`);
+</script>
